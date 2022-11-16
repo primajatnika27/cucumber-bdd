a04fa17d7ed2e5872db87af5fed91917c1aae5c9
Prima Jatnika <prima.jatnika@majoo.id>
2022-11-16 20:33:49 +0700

    HTML Report

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Features", glue = {"StepDefinition"})
+@CucumberOptions(features = "Features", glue = {"StepDefinition"}, plugin = { "pretty", "html:target/cucumber-reports" })
 public class Runner {
 
 }
